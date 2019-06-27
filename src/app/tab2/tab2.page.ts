376b758fc2556359211ec64a5df0a9dbc316da60
Tanzirul Haque <tanzirul.haque@outlook.com>
2019-06-27 15:09:49 +0600

    dynamic list added

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,24 +6,58 @@ import { IonItem, IonItemSliding } from '@ionic/angular';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
+
 export class Tab2Page {
 
   public itemColor = 'royalblue';
   public favoriteColor = 'royalblue';
   public starColor = 'royalblue';
-  constructor() { }
+  public notificationList:any = [];
+  constructor() { 
+    this.notificationList.push({
+      Id      : 'item101',
+      Class   : 'class 1',
+      Title   : 'Notification 1',
+      Time    : '06:45 PM',
+      Status  : 'unread'
+    },
+    {
+      Id      : 'item102',
+      Class   : 'class 2',
+      Title   : 'Notification 2',
+      Time    : '06:47 PM',
+      Status  : 'unread'
+    },
+    {
+      Id      : 'item103',
+      Class   : 'class 3',
+      Title   : 'Notification 3',
+      Time    : '06:55 PM',
+      Status  : 'unread'
+    });
+  }
 
-  favorite() {
-    // console.log("favorite called");
-    // console.log(itemSlide);
-    this.favoriteColor = 'green';
-    this.itemColor = 'green';
-    this.starColor = 'royalblue';
+  favorite(notification : any) {
+    console.log(notification);
+
+    var index = this.notificationList.findIndex(x => x.Id === notification.Id);
+    notification.Status = "unread";
+    this.notificationList.slice(index,1, {notification})
+    console.log(this.notificationList);
+    /* notificationId.favoriteColor = 'green';
+    notificationId.itemColor = 'green';
+    notificationId.starColor = 'royalblue'; */
   };
 
-  star() {
-    this.starColor = 'red';
-    this.itemColor = 'red';
-    this.favoriteColor = 'royalblue';
+  star(notification : any) {
+    console.log(notification);
+    var index = this.notificationList.findIndex(x => x.Id === notification.Id);
+    notification.Status = "read";
+    this.notificationList.slice(index,1, {notification})
+    console.log(this.notificationList);
+    /* 
+    notificationId.starColor = 'red';
+    notificationId.itemColor = 'red';
+    notificationId.favoriteColor = 'royalblue'; */
   };
 }
